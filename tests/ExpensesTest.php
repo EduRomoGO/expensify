0123ed02046f49ce602b0db123f648f179ae6009
Eduardo Romo <EduRomoGO@MacBook-Pro-de-Eduardo.local>
2015-07-14 19:03:11 +0200

    red

--- tests/ExpensesTest.php
+++ tests/ExpensesTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Expense;
 
 class ExpensesTest extends TestCase
 {
@@ -18,6 +19,24 @@ class ExpensesTest extends TestCase
 	{
 		$this->post('/expenses', ['title'=> 'ropa', 'amount' => 20])
 				 ->seeInDatabase('expenses', ['title'=> 'ropa', 'amount' => 20], $connection = null);
+	}
+
+	public function testSeeExpenses()
+	{
+		$this->seedDatabase();
+
+		$response = $this->call('GET', '/expenses');
+		$expenses = $response->getData();
+
+		$this->assertCount(2, $expenses);
+	}
+
+
+	private function seedDatabase ()
+	{
+		Expense::truncate();
+		$this->call('POST', '/expenses', ['title'=> 'ropa', 'amount' => 20]);
+		$this->post('/expenses', ['title'=> 'juguetes', 'amount' => 30]);
 	}
 
 }
