1dac9ae7ec91abd3b92c96779013df0415c5c97b
Eduardo Romo <EduRomoGO@MacBook-Pro-de-Eduardo.local>
2015-07-15 16:40:17 +0200

    Red

--- tests/acceptance/MainRouteCept.php
+++ tests/acceptance/MainRouteCept.php
@@ -7,3 +7,9 @@ $I->wantTo('check that main route has the new expense form');
 $I->amOnPage('/');
 $I->seeElement(['css' => 'form div input'], ['name' => 'title']);
 $I->seeElement(['css' => 'form div input'], ['name' => 'amount']);
+
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('check that the new expense form has a button to send the it');
+$I->amOnPage('/');
+$I->seeElement(['css' => 'button'], ['name' => 'createExpense']);
