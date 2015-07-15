27392552eb9c1a4155a30e2ff36fe338ee99d5a0
Eduardo Romo <EduRomoGO@MacBook-Pro-de-Eduardo.local>
2015-07-15 21:00:11 +0200

    Refactor:   * Combined the fetched and the created expenses views   * Now after creating an expense, we see that expense, and after fetching all the expenses, when we create another expense, we see an updated view of all the expenses, not just the last one created

--- public/js/bb.js
+++ public/js/bb.js
@@ -35,67 +35,32 @@
 
 
 
-
-  var FetchedExpensesView = Backbone.View.extend({
-    
-    el: 'body',
-    
-    events: {
-      'click button#see': 'seeExpenses',
-    },
-
-    initialize: function(){
-      _.bindAll(this, 'render', 'seeExpenses', 'appendExpense');
-      FetchedExpenses.bind('update', this.render);
-    },
-
-    render: function() {
-      var self = this;
-      $('#expenses-fetched-home').remove();
-      $(this.el).append("<section id='expenses-fetched-home'><ul class='fetched'></ul></section>");
-      _(FetchedExpenses.models).each(function(expense){
-        self.appendExpense(expense);
-      }, this);
-    },
-
-    seeExpenses: function() {
-      FetchedExpenses.fetch();
-    },
-
-    appendExpense: function (expense) {
-      var expenseView = new ExpenseView({
-        model: expense
-      });
-      $('ul', this.el).append(expenseView.render().el);
-    }
-
-  });
-
-  var FetchedExpensesListView = new FetchedExpensesView();
-
-
-
-
   var ExpensesView = Backbone.View.extend({
     
     el: $('body'),
 
     events: {
-      'click button#add': 'addExpense'
+      'click button#add': 'addExpense',
+      'click button#see': 'seeExpenses',
     },
 
     initialize: function(){
-      _.bindAll(this, 'render', 'addExpense', 'appendExpense');
+      _.bindAll(this, 'render', 'addExpense', 'appendExpense', 'seeExpenses');
 
       this.collection = new ExpenseList();
-      this.collection.bind('add', this.appendExpense);
+      this.collection.bind('add', this.render);
+      FetchedExpenses.bind('update', this.render);
 
       this.render();
     },
 
     render: function(){
       var self = this;
-      $(this.el).append("<section id='expenses-created-home'><ul></ul></section>");
+      $('#expenses-home').remove();
+      $(this.el).append("<section id='expenses-home'><ul></ul></section>");
+      _(FetchedExpenses.models).each(function(expense){
+        self.appendExpense(expense);
+      }, this);
       _(this.collection.models).each(function(expense){
         self.appendExpense(expense);
       }, this);
@@ -113,6 +78,10 @@
         model: expense
       });
       $('ul', this.el).append(expenseView.render().el);
+    },
+
+    seeExpenses: function() {
+      FetchedExpenses.fetch();
     }
 
   });
